821ac1d7d5aa8b17a3d81f639ebe14409baad108
Sr_L3M0N <italocarcheri@gmail.com>
2024-11-14 08:48:40 -0500

    menu principal arreglado

--- ConsoloApp1/Cita.cs
+++ ConsoloApp1/Cita.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoloApp1
 {
-    internal class Cita
+    public class Cita
     {
+        public int Num { get; set; }
+        internal Estudiante Estudiante { get; set; }
+        public string Enf { get; set; }
+        public double Pre { get; set; }
+
+        public override string ToString()
+        {
+            return $"Número: {Num}, Enfermedad: {Enf}, Estudiante: {Estudiante.Nombre}, Universidad: {Estudiante.Universidad}, Precio: {Pre}";
+        }
     }
+
 }
--- ConsoloApp1/Program.cs
+++ ConsoloApp1/Program.cs
@@ -9,34 +9,34 @@ namespace GestionCitasMedicas
         {
             
             Cita[] citas = new Cita[100];
-            int totalCitas = 0; // Contador para las citas creadas
+            int totalCitas = 0; 
 
             int opcion;
             do
             {
-                Console.WriteLine("\n--- Menú de Gestión de Citas Médicas ---");
-                Console.WriteLine("1. Crear Cita");
-                Console.WriteLine("2. Listar Citas");
-                Console.WriteLine("3. Modificar Masivo de Universidades");
-                Console.WriteLine("4. Fin");
+                Console.WriteLine(" Menú de citas medicas ");
+                Console.WriteLine("1) Crear ");
+                Console.WriteLine("2) Listar ");
+                Console.WriteLine("3) Modificar ");
+                Console.WriteLine("4) Fin");
                 Console.Write("Seleccione una opción: ");
                 opcion = int.Parse(Console.ReadLine());
 
                 switch (opcion)
                 {
                     case 1:
-                        // Llamada al método CrearCita de CitaUtil
+                        
                         CitaUtil.CrearCita(citas, ref totalCitas);
                         break;
 
                     case 2:
-                        // Llamada al método ListarCitas de CitaUtil
+                        
                         CitaUtil.ListarCitas(citas, totalCitas);
                         break;
 
                     case 3:
-                        // Llamada al método ModificarMasivoUniversidad de CitaUtil
-                        Console.Write("Ingrese el texto a buscar en universidades: ");
+                        
+                        Console.Write("Ingrese el texto a buscar : ");
                         string textoBuscar = Console.ReadLine();
                         Console.Write("Ingrese el texto de reemplazo: ");
                         string textoReemplazo = Console.ReadLine();
