3f5acd49e480d04de5ae0cab46228564225b306b
Sr_L3M0N <italocarcheri@gmail.com>
2024-11-14 08:12:17 -0500

    segundo commit main

---
+++ ConsoloApp1/Estudiante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoloApp1
+{
+    internal class Estudiante
+    {
+        public int Cod { get; set; }
+        public string Nom { get; set; }
+        public string Uni { get; set; }
+
+        public override string ToString()
+        {
+            return $"Código: {Cod}, Nombre: {Nom}, Universidad: {Uni}";
+        }
+    }
+
+}
