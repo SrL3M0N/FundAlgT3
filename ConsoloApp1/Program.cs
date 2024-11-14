f1dddbec2e84ca4ec6b9ae29e042d136343285ee
Sr_L3M0N <italocarcheri@gmail.com>
2024-11-14 08:33:50 -0500

    menu principal

--- ConsoloApp1/Program.cs
+++ ConsoloApp1/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoloApp1;
+using System;
+
+namespace GestionCitasMedicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cita[] citas = new Cita[100];
+            int totalCitas = 0; // Contador para las citas creadas
+
+            int opcion;
+            do
+            {
+                Console.WriteLine("\n--- Menú de Gestión de Citas Médicas ---");
+                Console.WriteLine("1. Crear Cita");
+                Console.WriteLine("2. Listar Citas");
+                Console.WriteLine("3. Modificar Masivo de Universidades");
+                Console.WriteLine("4. Fin");
+                Console.Write("Seleccione una opción: ");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        // Llamada al método CrearCita de CitaUtil
+                        CitaUtil.CrearCita(citas, ref totalCitas);
+                        break;
+
+                    case 2:
+                        // Llamada al método ListarCitas de CitaUtil
+                        CitaUtil.ListarCitas(citas, totalCitas);
+                        break;
+
+                    case 3:
+                        // Llamada al método ModificarMasivoUniversidad de CitaUtil
+                        Console.Write("Ingrese el texto a buscar en universidades: ");
+                        string textoBuscar = Console.ReadLine();
+                        Console.Write("Ingrese el texto de reemplazo: ");
+                        string textoReemplazo = Console.ReadLine();
+                        CitaUtil.ModificarMasivoUniversidad(citas, totalCitas, textoBuscar, textoReemplazo);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Fin del programa.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+
+            } while (opcion != 4);
+        }
+    }
+}
